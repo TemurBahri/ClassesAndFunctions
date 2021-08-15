8aebe690693e90d17c22953bdeb4ab7940ce797e
Temur <77517397+TemurBahri@users.noreply.github.com>
2021-08-15 18:52:06 +0300

    Kotlin Learning 3

--- app/src/main/java/com/timurbahri/kotlinclassesandfunctions/MainActivity.kt
+++ app/src/main/java/com/timurbahri/kotlinclassesandfunctions/MainActivity.kt
@@ -2,6 +2,7 @@ package com.timurbahri.kotlinclassesandfunctions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,19 +10,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         test()
-        test2()
+        mySum(5,5)
+        myMultiply(50,40)
+
+        val hommer = Simpson("Homer Simpson",50,"Nuclear")
+        hommer.hairColor = "Yellow"
+
+
+        //hommer.age = 60
+        //hommer.job = "Nuc"
+        //hommer.name ="Hommer"
+        println(hommer.hairColor)
     }
 
     fun  test(){
         println("test functions")
+    }
+
+
+    //Input & Return
+    fun mySum(a:Int,b:Int){
+        println(a+b)
+    }
+
+    fun myMultiply(x:Int,y:Int) : Int {
+        return x * y
 
     }
 
-    fun  test2(){
-        println("test functions")
-
+    fun helloKotlin(view : View){
+        println("hello Kotlin")
     }
-
 
 
 
---
+++ app/src/main/java/com/timurbahri/kotlinclassesandfunctions/Simpson.kt
@@ -0,0 +1,23 @@
+package com.timurbahri.kotlinclassesandfunctions
+
+class Simpson (var name:String,var age:Int,var job:String) {
+
+    var hairColor = ""
+
+
+    //Property
+    /*
+    var age = 0
+    var name = ""
+    var job = ""
+
+    constructor(ageInput:Int,nameInput:String,jobInput:String) {
+        this.age = ageInput
+        this.name = nameInput
+        this.job = jobInput
+
+     */
+
+
+    }
+
